538dee7fecb298b06b8f51b3a23f8e7a74fcdac0
Anushka Jain <102728955+Anushkajain22@users.noreply.github.com>
2025-05-29 22:10:32 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
